3386041bff200bd0b992ad470183e0f3d10bff53
santhosh803 <sandyceo007@gmail.com>
2026-08-24 23:43:00 +0530

    Fix NameError in urls.py by restoring from procurement import pages

--- procuremcp/urls.py
+++ procuremcp/urls.py
@@ -7,6 +7,8 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 
+from procurement import pages
+
 admin.site.site_header = "ProcureMCP Administration"
 admin.site.site_title = "ProcureMCP Admin"
 admin.site.index_title = "Procurement Operations & Agent Control"
